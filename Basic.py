5ebd93a5bf2f1ed8a632c4879cda6ce2d9c8ab79
Vader2005 <survivalmeical@gmail.com>
2022-05-24 21:21:10 +1200

    name change

--- Basic.py
+++ Basic.py
@@ -3,7 +3,7 @@ from tkinter import *
 root = Tk()
 
 def click():
-    Label(root, text="Hello There").place(x=100, y=100)
+    Label(root, text="Hello What is your name?").place(x=100, y=100)
 
 # Button
 Button1 = Button(root, text="Click me", command=click).place(x=250, y=250)
