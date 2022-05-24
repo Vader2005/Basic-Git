226b1c1f6459cd8c16b7f4937aaf59ad7efddf98
Vader2005 <survivalmeical@gmail.com>
2022-05-24 21:18:41 +1200

    init

---
+++ Basic.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+root = Tk()
+
+def click():
+    Label(root, text="Hello There").place(x=100, y=100)
+
+# Button
+Button1 = Button(root, text="Click me", command=click).place(x=250, y=250)
+
+root.geometry("500x500")
+root.mainloop()
